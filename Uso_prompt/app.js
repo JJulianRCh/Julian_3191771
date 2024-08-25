082044f25aec50fa7fa5a1e0ef520b8232ab2d41
Julian <julianrodriguez27@aragon.unam.mx>
2024-08-24 20:50:07 -0600

    Agregando codigos

---
+++ Uso_prompt/app.js
@@ -0,0 +1,6 @@
+//Utilizando la funcion prompt
+let nombre = prompt("Ingrese su nombre");
+let edad = parseInt("Ingrese su edad");
+//Mostrando en la consola
+console.log("Su nombre es", nombre);
+console.log("Tiene %d anios de edad", edad);
