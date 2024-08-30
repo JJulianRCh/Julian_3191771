3af7d1cd48843402d332fa1603d68fc9e77ec9aa
Julian <julianrodriguez27@aragon.unam.mx>
2024-08-30 00:21:21 -0600

    Funcion suma

---
+++ funcion_suma/app.js
@@ -0,0 +1,9 @@
+// Una funcion que realiza una suma
+function suma(a, b) {
+    return a + b;
+}
+
+let a = parseInt(prompt("Ingrese un valor"));
+let b = parseInt(prompt("Ingrese otro valor"));
+let resultado = a + b;
+console.log("La suma de %d mas %d es igual a %d", a, b, resultado);
